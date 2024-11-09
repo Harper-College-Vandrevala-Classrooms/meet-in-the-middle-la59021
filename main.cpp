3773fe4ce02ebf0345d866c7a211b159b549ab14
Alehmann04 <la59021@mail.harpercollege.edu>
2024-11-09 11:35:43 -0600

    Median and Mean working

--- main.cpp
+++ main.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <vector>
 using namespace std;
+
+vector<int> makeArray(string rawString) {
+    string digits;
+    vector<int> contents;
+    for (auto i : rawString) {
+        if (i != ',') {
+            digits += i;
+        }
+        else if (i == ',' && !digits.empty()) {
+            contents.insert(contents.end(), stoi(digits));
+            digits = "";
+        }
+        else if (i == ',' && digits.empty()) {}
+    }
+    if (!digits.empty()) {
+        contents.insert(contents.end(), stoi(digits));
+    }
+    if (contents.size() == 0) {
+        cout << "Error: no numbers found" << endl;
+    }
+    return contents;
+}
+
 
 void printToFile(string directory) {
     ifstream myfile(directory);
@@ -10,52 +34,52 @@ void printToFile(string directory) {
         return;
     }
 
-    string mytext, contents;
-    while (getline(myfile, mytext)) {
-        contents += mytext;
+    string line, raw;
+    while (getline(myfile, line)) {
+        raw += line;
+        raw += ",";
     }
     myfile.close();
 
-    if (contents.empty()) {
+    if (raw.empty()) {
         cout << "Error reading file: empty contents" << endl;
         return;
     }
 
-    cout << contents << endl;
-    string presum;
-    int sum = 0;
-    int count = 0;
-    for (int x = 0; x < contents.size(); x++) {
-        char input = contents[x];
-        if (input != ',') {
-            presum += input;
-        }
-        else if (input == ',') {
-            if (!presum.empty()) {
-                sum += stoi(presum);
-                count++;
-                presum = "";
-            }
-        }
-    }
-    if (!presum.empty()) {
-        sum += stoi(presum);
-        count++;
+    vector<int> contents = makeArray(raw);
+    int sum = 0, count = 0;
+    double median = 0, mean = 0;
+
+    // sum
+    for (int i : contents) {
+        sum += i;
     }
 
-    if (count == 0) {
-        cout << "Error: no numbers found" << endl;
-        return;
+    // count
+    count = contents.size();
+
+    //median
+    if (count % 2 == 0) {
+        int middleUp = contents[(count + 1) / 2];
+        int middleDown = contents[(count - 1) / 2];
+        median = (middleUp + middleDown) / 2;
     }
+    else {
+        median = contents[count / 2];
+    }
+
+    //mean
+    mean = sum/count;
 
     cout << "Sum: " << sum << endl;
     cout << "Count: " << count << endl;
-    cout << "Avg: " << (double)sum / count << endl;
+    cout << "Median: " << median << endl;
+    cout << "Mean: " << mean << endl;
 }
 
 int main() {
     string myfile;
     cout << "Enter the name of the file: ";
-    cin >> myfile;
+    getline(cin, myfile);
     printToFile(myfile);
 }
